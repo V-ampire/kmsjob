f08858d6c737d0aca3d8223fc4bacba2120f9624
V-ampire <webjob010@gmail.com>
2021-06-03 09:59:57 +1000

    fix install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -27,23 +27,23 @@ pytnon backend/manage.py migrate
 
 # Создаем индексы для текстового поиска
 echo Creating database indexes...
-PGPASSWORD=$DB_PASSWORD psql -U $DB_USER --set=DB_NAME=$DB_NAME -f shell_scripts/create_db_index.sql
+PGPASSWORD=$DB_PASSWORD psql -U $DB_USER --set=DB_NAME=$DB_NAME -f scripts/create_db_index.sql
 
 # Собираем статику
 pytnon backend/manage.py collectstatic
 
 # Подставляем путь до проекта в конфиги и скрипты
 echo Configure project path and domain...
-sed -i "s~template_path~$project_path~g" nginx/site.conf systemd/gunicorn.service
-sed -i "s~template_domain~$project_domain~g" nginx/site.conf
+sed -i "s~template_path~$project_path~g" nginx/kmsjob.conf systemd/kmsjob.gunicorn.service
+sed -i "s~template_domain~$project_domain~g" nginx/kmsjob.conf
 sed -i "s~template_path~$project_path~g" scripts/run_parsers.sh
 sed -i "s~template_path~$project_path~g" scripts/run_cleaner.sh
 sed -i "s~template_path~$project_path~g" scripts/crontab.txt
 
 # Подключаем сервера
 echo Enable servers...
-sudo ln -s $project_path/nginx/site.conf /etc/nginx/sites-enabled/
-sudo ln -s $project_path/systemd/gunicorn.service /etc/systemd/system/
+sudo ln -s $project_path/nginx/kmsjob.conf /etc/nginx/sites-enabled/
+sudo ln -s $project_path/systemd/kmsjob.gunicorn.service /etc/systemd/system/
 
 sudo systemctl daemon-reload
 sudo systemctl start gunicorn
